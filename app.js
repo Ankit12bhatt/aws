e2a8abc1de99f65af9ab12265ea9ed9812539d1e
Ankit12bhatt <ankitbhatt640@gmail.com>
2022-11-10 19:57:33 +0530

    nine commit

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express= require('express');
 const app=express();
+app.set('view engine', 'ejs')
 app.get("/", function(req, res){
-    res.send("Hello BabyDoll its working yeah!!!");
+    res.render("pages/index");
 })
 port= process.env.port || 3000;
 app.listen(port, function(){
     console.log("Listening  to you motherfyucing",port);
     console.log("HULLE HULARE HULLE HULEE HULEE 3000");
 })
-    
+     
