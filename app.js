ac422a7be8064a8125005d80668636c68c89b0c6
Ankit12bhatt <ankitbhatt640@gmail.com>
2022-11-14 15:49:32 +0530

    10th commit

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ app.set('view engine', 'ejs')
 app.get("/", function(req, res){
     res.render("pages/index");
 })
+app.get("/suchii", function(req, res){
+    res.send("hey babydoll whtsup");
+
+})
+app.get("/sikhimaru", function(req, res){
+   res.send("what a drag");
+})
+app.get("/stuti", function(req, res){
+    res.send("shadi kr k fas gaya yr, aacha khasa tha kawara");
+
+})
+
 port= process.env.port || 3000;
 app.listen(port, function(){
     console.log("Listening  to you motherfyucing",port);
