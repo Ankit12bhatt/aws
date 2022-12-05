c881b8a57db1379d06d6d7bc9ed4161760226c02
Ankit12bhatt <ankitbhatt640@gmail.com>
2022-12-05 11:08:50 +0530

    fina cimmit

--- app.js
+++ app.js
@@ -4,21 +4,11 @@ app.set('view engine', 'ejs')
 app.get("/", function(req, res){
     res.render("pages/index");
 })
-app.get("/suchii", function(req, res){
-    res.send("hey babydoll whtsup");
 
-})
-app.get("/sikhimaru", function(req, res){
-   res.send("what a drag");
-})
-app.get("/stuti", function(req, res){
-    res.send("shadi kr k fas gaya yr, aacha khasa tha kawara");
-
-})
 
 port= process.env.port || 3000;
 app.listen(port, function(){
-    console.log("Listening  to you motherfyucing",port);
-    console.log("HULLE HULARE HULLE HULEE HULEE 3000");
+    console.log("Listening  to you",port);
+    console.log(" 3000");
 })
      
